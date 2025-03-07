f9961293361e21bba358ec88fcc33bcc814dfd36
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-03-07 20:45:45 +0000

    Create duplicated_employees.py

---
+++ duplicated_employees.py
@@ -0,0 +1,10 @@
+import pandas as pd
+employee_data = {"Name":['Lee Cox','Matt Bond','Raven Adams','Jodie Green','Tammy McGuire','Lemon Jelly','Lee Cox','Raven Adams','Pamela Green'],
+                 "Position":['Computer Scientist','Financial Analyst','Accountant','Project Manager','Teacher','Lawyer','Computer Scientist','Accountant','Lawyer']}
+dataOne = {"Drive a car?":[True, True, True, False, False, True, True,True,True],
+           "Salary":[35000,30000,45000,50000,45000,55000,35000,45000,60000]}
+par1 = pd.DataFrame(employee_data)
+par2 = pd.DataFrame(dataOne)
+employee_df = par1.join(par2)
+duplicatedED = employee_df.duplicated()
+print(duplicatedED)
