6f6efe2aaea8d3085243804fcf5382da737aeb86
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-03-09 17:18:18 +0000

    Create Nov_Remote_1.py

---
+++ Nov_Remote_1.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+NovDF = pd.read_csv('November Feedback.csv')
+columnsToDelete3 = ['Anonymized Contact ID', 'Posting ID','Current Stage ID',
+       'Survey Id','How satisfied were you with our communication during the recruiting process? - e8c5e9ae-38de-4acb-855c-8d18039b1f5c',
+       'How satisfied were you with the interview materials you received? - 4158aafc-ce92-4d35-930f-2f5451455a4d',
+       'How satisfied are you with your interviews with the Hiring Manager and Team? - 605b085d-5bba-4668-babc-17dc8304e06c',
+       'How satisfied were you with the interview feedback you received? - e30211c6-d32d-4f89-95fc-dfb91bb47033',
+       'Do you have any thoughts or comments that you would like to share? - a611bc41-fd5f-4f91-a277-3b0a260c7401',
+       'How satisfied were you with your Screening call? - de09e7f8-4e8c-498d-9ccb-237788b735d0',
+       'How satisfied are you with the way you were treated? - 05c18285-2750-479f-8afc-d560b66f552c',
+       'Overall, how satisfied were you with your interviewing experience? - 693b0a81-d4e7-40a2-9135-7fc38fa43eaf',
+       'What suggestions would you make to improve our interview process? - 21866cc4-7d8f-47da-97df-9efdfcbe083d',
+       'How satisfied were you with the ease of the application process? - 513b2b55-b818-4f03-bf6f-e82b180c9e60','Stage - New lead', 'Stage - Reached out', 'Stage - Responded',
+       'Stage - New applicant', 'Stage - Under review',
+       'Stage - Recruiter Screen', 'Stage - Hiring Manager Screen',
+       'Stage - 1st Round', 'Stage - 2nd Round', 'Stage - Decision',
+       'Stage - Offer','Survey Name','Survey Location Type','Candidate Selected Location','Commitment / Work Type']
+NovDF = NovDF.drop(columnsToDelete3, axis=1)
+NovRemote1 = NovDF.where(NovDF['Location'] == 'UK (Remote)')
+NovRemote = NovRemote1.dropna()
+print(NovRemote.to_string())
