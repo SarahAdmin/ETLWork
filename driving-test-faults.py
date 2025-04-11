93929bdf9935ebc9e79fcfeb453116de9e6f546a
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-04-11 22:58:49 +0100

    Update driving-test-faults.py

--- driving-test-faults.py
+++ driving-test-faults.py
@@ -3,6 +3,6 @@ import pandas as pd
 driveDF = pd.read_csv('drt121f-car-driving-tests-top-10-faults.csv',encoding='latin-1')
 columnToDelete = ['Period']  
 driveOne = driveDF.drop(columnToDelete,axis=1) 
-driveOne = driveDF.duplicated() 
+driveTwo = driveOne.duplicated() 
 
-print(driveOne)
+print(driveTwo)
