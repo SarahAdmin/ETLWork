3a9926401a1d38367e559c6802311833966697fc
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-04-15 15:18:49 +0100

    Update Members-Project.py

--- Members-Project.py
+++ Members-Project.py
@@ -3,4 +3,4 @@ members_df = pd.read_excel('Excel example data (2).xlsx',sheet_name='Data 2')
 duplicatedMembers = members_df.duplicated()
 print(duplicatedMembers)
 
-// the output returns boolean values // 
+
