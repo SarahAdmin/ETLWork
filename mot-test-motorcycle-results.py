777d799f84915af9a8bd76d861ad3d4c2e5712b3
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2025-04-28 20:31:58 +0100

    Update mot-test-motorcycle-results.py

--- mot-test-motorcycle-results.py
+++ mot-test-motorcycle-results.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-motDF = ('dvsa-mot-01-mot-test-results-by-class-of-vehicle.csv',encoding='latin-1')
+motDF = pd.read_csv('dvsa-mot-01-mot-test-results-by-class-of-vehicle.csv',encoding='latin-1')
 motorcycle_results = motDF.where(motDF['Class'] == 'Classes 1 & 2: Motorcycles').dropna() 
 print('MOT Motorcycle Results')
 print(motorcycle_results.to_string())
